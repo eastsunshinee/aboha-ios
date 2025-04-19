e9e9d13ccca9a6c08604fc559a853d20605e5f3f
eastsunshinee <eastsunshinee@gmail.com>
2025-04-19 16:02:50 +0900

    Initial Commit

---
+++ Aboha/AbohaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbohaApp.swift
+//  Aboha
+//
+//  Created by 김동현 on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AbohaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
